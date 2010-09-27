5febdf20148611062428d572cc6a8d782b1de9cc
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2010-09-27 21:21:53 +0100

    emacs lisp functions.

---
+++ funcs.el
@@ -0,0 +1,16 @@
+;;;move function somewhere else
+(defun perl-guess-package (&optional file)
+  ( let* ( (file (or file buffer-file-name) )
+         ( file (replace-regexp-in-string ".*/\\(?:lib\\|t\\)/" "" file) )
+         (file (replace-regexp-in-string "\\.pm$" "" file) )
+         (file (replace-regexp-in-string "/" "::" file) ) )
+    file ) )
+
+(defun find-attribute-name()
+  ( save-excursion 
+    ( re-search-backward "has .+ =>" )
+     (let* ( (data (match-string-no-properties 0 ))
+             (data (replace-regexp-in-string "^has " "" data) )
+             (data (replace-regexp-in-string " =>$" "" data) )
+             )
+           data ) ) )
